40f9d1bc0167b0fa9ee03dceebf05c43d0be3e7c
Kevin Mehall <kevin.mehall@3drobotics.com>
2015-12-17 21:04:37 -0800

    https://xkcd.com/1296/

--- calibrate.py
+++ calibrate.py
@@ -21,16 +21,18 @@ pos_lpf = 0
 def msg_recv(msg_id, payload):
     global pos_lpf
     if msg_id == 0x02:
-        pos_lpf = pos_lpf * 0.999 + (payload[0] | payload[1] << 8)*0.001
+        pos_lpf = pos_lpf * 0.99 + (payload[0] | payload[1] << 8)*0.01
         print "Position: %5.0f"%pos_lpf
     else:
         print msg_id, str(payload).encode("hex")
 
 
 
-ser.write(CMD_MSTAT, [MSTAT_EN | MSTAT_PARK_OVERRIDE])
-ser.write(CMD_SET_IQ, [0x00, 0x00])
-ser.write(CMD_SET_ID, [0x00, 0x20])
-
 while True:
+    ser.write(CMD_MSTAT, [MSTAT_EN | MSTAT_PARK_OVERRIDE])
+    ser.write(CMD_SET_IQ, [0x00, 0x00])
+    ser.write(CMD_SET_ID, [0x00, 0x10])
     ser.read(msg_recv)
+    time.sleep(0.001)
+    
+    
--- hydraclient.py
+++ hydraclient.py
@@ -6,10 +6,37 @@ import time
 
 ser = HydraSerial(argv[1],115200)
 
+def motor_enable():
+    ser.write(0x03, [0x01])
+
+def set_iq(val):
+    if val > 0x7fff:
+        val = 0x7fff
+    if val < -0x7fff:
+        val = -0x7fff
+    ser.write(0x01, struct.pack( "<h", val))
+    
+def set_id(val):
+    ser.write(0x04, struct.pack( "<h", val))
+
+position = 0
+
 def msg_recv(msg_id, payload):
     if msg_id == 0x02:
-        print "theta: %f" % (degrees(struct.unpack("H", payload)[0] *(2.*pi)/65536.))
-        ser.write(0x01, struct.pack('<h', 2000*sin(time.time())))
+        position = (payload[0] | payload[1] << 8)
+        print "Position:", position
+    else:
+        print msg_id, str(payload).encode("hex")
+        
+
+last_err = 0
+i_err = 0
+
+motor_enable()
+
 
 while True:
+    set_iq(int(argv[2]))
+    set_id(int(argv[3]))  
     ser.read(msg_recv)
+    time.sleep(0.001)
